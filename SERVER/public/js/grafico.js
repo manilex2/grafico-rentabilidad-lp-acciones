7ff321584e4054cc90e6c1278d0fb68a0fb695ec
Manuel Pereira <manilex2@gmail.com>
2022-10-05 18:18:46 -0500

    Modificado formato de graficos de rentabilidad

--- SERVER/public/js/grafico.js
+++ SERVER/public/js/grafico.js
@@ -9,20 +9,20 @@ const plugin = {
     id: 'custom_canvas_background_image',
     beforeDraw: (chart) => {
         if (image.complete) {
-        const ctx = chart.ctx;
+        let contexto = chart.ctx;
         const {top, left, width, height} = chart.chartArea;
         const x = left + width / 2 - image.width / 2;
         const y = top + height / 2 - image.height / 2;
-        ctx.drawImage(image, x, y);
+        contexto.drawImage(image, x, y);
         } else {
-        image.onload = () => chart.draw();
+            image.onload = () => chart.draw();
         }
     }
 };
 
 
 
-function totalChart(ctx, extraerdatos){
+function totalChart(extraerdatos){
     var matriz1 = [];
     var matriz2 = [];
     var matriz3 = [];
@@ -35,7 +35,7 @@ function totalChart(ctx, extraerdatos){
         for (let i = 0; i < item.length; i++) {
             const element = item[i];
             if (element.d30) {
-                matriz1.push(element.d30);
+                matriz1.push((element.d30*100).toFixed(0));
             }
         }
         return matriz1;
@@ -45,7 +45,7 @@ function totalChart(ctx, extraerdatos){
         for (let i = 0; i < item.length; i++) {
             const element = item[i];
             if (element.m3) {
-                matriz2.push(element.m3);
+                matriz2.push((element.m3*100).toFixed(0));
             }
         }
         return matriz2;
@@ -55,7 +55,7 @@ function totalChart(ctx, extraerdatos){
         for (let i = 0; i < item.length; i++) {
             const element = item[i];
             if (element.m6) {
-                matriz3.push(element.m6);
+                matriz3.push((element.m6*100).toFixed(0));
             }
         }
         return matriz3;
@@ -65,7 +65,7 @@ function totalChart(ctx, extraerdatos){
         for (let i = 0; i < item.length; i++) {
             const element = item[i];
             if (element.a1) {
-                matriz4.push(element.a1);
+                matriz4.push((element.a1*100).toFixed(0));
             }
         }
         return matriz4;
@@ -159,7 +159,7 @@ function totalChart(ctx, extraerdatos){
             datasets: total,
             labels: ['Conservador', 'Optimista', 'Pesimista'],
         },
-        plugins: [plugin],
+        plugins: [plugin, ChartDataLabels],
         options: {
             layout: {
                 padding: {
@@ -210,6 +210,19 @@ function totalChart(ctx, extraerdatos){
                     color: '#FFF',
                     padding: 30
                 },
+                datalabels: {
+                    color: '#FFF',
+                    anchor: 'end',
+                    align: 'top',
+                    offset: 10,
+                    font: {
+                        weight: 'bold'
+                    },
+                    formatter: function (value, context) {
+                        console.log(context.dataset.data[context.dataIndex]);
+                        return context.dataset.data[context.dataIndex] + '%';
+                    },
+                },
                 /*legend: {
                     position: 'bottom',
                     labels: {
@@ -224,6 +237,7 @@ function totalChart(ctx, extraerdatos){
                     }
                 },*/
                 tooltip: {
+                    enabled: false,
                     backgroundColor: '#0584f6',
                     titleFontSize: 20,
                     xPadding: 20,
@@ -234,7 +248,6 @@ function totalChart(ctx, extraerdatos){
                 },
             }
         },
-        
     });
 }
 
@@ -244,7 +257,7 @@ async function renderizarCharts() {
         return res.json();
     }).then((json) => {
         var extraerDatos = json;
-        totalChart(ctx, extraerDatos);
+        totalChart(extraerDatos);
     });
 }
 renderizarCharts();
